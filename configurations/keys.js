803ac1b95d4939c6ac3b41358fad59d253ea37bf
hibamouhoub <hibamouhoub0@gmail.com>
2021-01-05 00:27:40 +0100

    c

--- configurations/keys.js
+++ configurations/keys.js
@@ -3,7 +3,6 @@ dbPassword = 'mongodb+srv://hiba:'+ encodeURIComponent('fuckthemall-1') + '@e-au
 
 module.exports = {
     MongoURI: dbPassword,
-
     GoogleClientID: "439365422515-tj9oc8o5g9vc2on26hmu3i4907qidvjl.apps.googleusercontent.com",
     GoogleClientSecret: "sO1CF0GVweIOx8i9NZVuPNqL"
 };
